61ab8efc8c3cbad5ae6fb3c2e8a9d6fbe1379d2c
Jack Franklin <jack@jackfranklin.net>
2015-12-07 17:51:12 +0000

    Add CSS file

--- config.js
+++ config.js
@@ -18,6 +18,7 @@ System.config({
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "chai": "npm:chai@3.4.1",
     "core-js": "npm:core-js@1.2.6",
+    "css": "github:systemjs/plugin-css@0.1.20",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
